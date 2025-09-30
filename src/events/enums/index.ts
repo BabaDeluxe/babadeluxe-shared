c964c75bfd55758fedcc044a206b57403eb48d95
simwai <16225108+simwai@users.noreply.github.com>
2025-09-30 13:19:18 +0200

    feat: :sparkles: Added back ModelOption and ActiveChatItemEmitter

--- src/events/enums/index.ts
+++ src/events/enums/index.ts
@@ -4,4 +4,3 @@
 
 export * from './prompt-events'
 export * from './setting-events-enum'
-export * from './webview-enum'
---
+++ src/model-option.ts
@@ -0,0 +1,5 @@
+export type ModelOption = {
+  id: string
+  name: string
+  icon: string
+}
