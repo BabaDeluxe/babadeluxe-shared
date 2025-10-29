a7e4f65004bd067e68ac02f75560751c3ba34593
simwai <16225108+simwai@users.noreply.github.com>
2025-10-29 01:04:10 +0100

    refactor: :coffin: Removed unused file

--- src/execute-prompt-result.ts
+++
@@ -1,7 +0,0 @@
-export type ExecutePromptResult = {
-  readonly promptId: number
-  readonly promptName: string
-  readonly processedMessage: string
-  readonly variables: Record<string, unknown>
-  readonly executedAt: Date
-}
